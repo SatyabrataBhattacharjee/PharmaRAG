a3117dd7312bc03df8837554841e919db5566e59
SatyabrataBhattacharjee <satyabratbhattacharya1408@gmail.com>
2025-07-07 15:46:27 +0530

    Update load_retriever.py

--- src/load_retriever.py
+++ src/load_retriever.py
@@ -1,1 +1,7 @@
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+
+embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+vectorstore = FAISS.load_local("my_faiss_store", embeddings=embedding_model, allow_dangerous_deserialization=True)
+retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
 
